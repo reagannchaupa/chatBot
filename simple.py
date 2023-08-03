a66d5455bc25acbf0931572781d583e250008545
Reagan Nchaupa <reagan180@hotmail.com>
2023-08-03 10:35:59 +0200

    Updated file

--- simple.py
+++ simple.py
@@ -1,29 +1,38 @@
-# Is a simple chatbot demonstration
+# Demonstrates a simple interaction with a chatbot
 
 import openai
 
-# Setup the API
-openai.api_key = "OPENAI_API_KEY"
+
+def main():
+    # Setup the API and start chat
+    openai.api_key = "OPENAI_API_KEY"
+    chat()
 
 
-# Define a function to interact with the bot
+# Define how to get a response from chatbot
 def ask_chatbot(prompt):
     response = openai.Completion.create(
         engine="gpt-3.5-turbo",
-    prompt=prompt,
-    max_tokens=150,
+        prompt=prompt,
+        max_tokens=150,
     )
-    return response['choices'][0]['text'].strip()
+    return response["choices"][0]["text"].strip()
 
 
-# Use the function to chat with the bot
-try: 
-    while True:
-        user_input = input("You: ")
-        if user_input.lower() in ['exit', 'quit', 'bye']:
-            print("Bot: Goodbye!")
-            break
-        bot_response = ask_chatbot("You: " + user_input + "\nBot:")
-        print(f"Bot: {bot_response}")
-except:
-    print("Something went wrong")
+# Use that function to chat with the chatbot
+def chat():
+    # Continuously try to get a response from chatbot until user stops.
+    try:
+        while True:
+            user_input = input("You: ")
+            if user_input.lower() in ["exit", "quit", "bye"]:
+                print("Bot: Goodbye!")
+                break
+            bot_response = ask_chatbot("You: " + user_input + "\nBot:")
+            print(f"Bot: {bot_response}")
+    except:
+        print("Something went wrong!")
+
+
+if __name__ == "__main__":
+    main()
